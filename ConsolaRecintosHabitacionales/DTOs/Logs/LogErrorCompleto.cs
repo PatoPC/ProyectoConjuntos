e1b13190fb0204361ae9302bafd40f7e337cce2b
PatoPC <patrico_pc@yahoo.es>
2023-01-15 12:44:53 -0500

    Se añade tipo areas a departamentos (bodega, parquederos, etc). Y se añade la vista para los logs del aplicativo.

---
+++ ConsolaRecintosHabitacionales/DTOs/Logs/LogErrorCompleto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Logs
+{
+    public class LogErrorCompleto
+    {
+        public Guid IdLogExcepciones { get; set; }
+        public Guid IdPersona { get; set; }
+        public Guid? IdUsuario { get; set; }
+        public string NombrePersona { get; set; }
+        public string Identificacion { get; set; }
+        public string IdEmpleado { get; set; }
+        public string Metodo { get; set; }
+        public string Entidad { get; set; }
+        public string Error { get; set; }
+        public DateTime FechaError { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ ConsolaRecintosHabitacionales/DTOs/Logs/ResultadoBusquedaLogDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOs.Logs
+{
+    public class ResultadoBusquedaLogDTO
+    {
+        public Guid IdLogExcepciones { get; set; }
+        public Guid? IdUsuario { get; set; }
+        public string Metodo { get; set; }
+        public string Entidad { get; set; }
+        public string Error { get; set; }
+        public DateTime FechaError { get; set; }
+        
+    }
+}
