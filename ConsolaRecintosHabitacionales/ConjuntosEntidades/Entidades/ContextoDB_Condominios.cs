9d314cb3e7670ee61b523d3f6dae6023ba2ba5b7
PatoPC <patrico_pc@yahoo.es>
2024-08-09 12:57:18 -0500

    Conexiones Casa

--- ConsolaRecintosHabitacionales/ConjuntosEntidades/Entidades/ContextoDB_Condominios.cs
+++ ConsolaRecintosHabitacionales/ConjuntosEntidades/Entidades/ContextoDB_Condominios.cs
@@ -43,9 +43,9 @@ namespace ConjuntosEntidades.Entidades
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                //optionsBuilder.UseSqlServer("server=DESKTOP-26QEGBC\\SQLEXPRESS;database=Condominios;persist security info=True;user id=AdminSQLUser;password=1915.*@Ort.;MultipleActiveResultSets=True");
+                optionsBuilder.UseSqlServer("server=DESKTOP-26QEGBC\\SQLEXPRESS;database=Condominios;persist security info=True;user id=AdminSQLUser;password=1915.*@Ort.;MultipleActiveResultSets=True");
 
-                optionsBuilder.UseSqlServer("server=PC_TI-Quito\\SQLEXPRESS;database=Papelera_Conjuntos;persist security info=True; Encrypt=False;user id=AdminSQLUser;password=1915.*@Ort.;MultipleActiveResultSets=True");
+                //optionsBuilder.UseSqlServer("server=PC_TI-Quito\\SQLEXPRESS;database=Papelera_Conjuntos;persist security info=True; Encrypt=False;user id=AdminSQLUser;password=1915.*@Ort.;MultipleActiveResultSets=True");
 			}
         }
 
